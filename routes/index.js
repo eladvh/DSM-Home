274caddc9a9b446595a3b6fa9f2ae10de6141b3b
eladvh <eladvh@gmail.com>
2018-04-10 01:32:31 +0300

    update routes file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var app     = express();
+var path    = require("path");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send('HOME');
+  //res.sendfile('public/index.html'); ;
+  res.status(200).send('Hi eladp!');
   //res.render('index', { title: 'Express' });
 
 });
