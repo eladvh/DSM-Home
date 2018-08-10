03e541070faa76b0c75952c948ea6c19bb1d9fa7
Eladvh <eladvh@gmail.com>
2018-08-10 18:49:31 +0300

    try again

--- routes/user.js
+++ routes/user.js
@@ -46,7 +46,7 @@ exports.signup = function(req, res){
 };
  //-----------------------------------------------login page call------------------------------------------------------
  exports.login = function(req, res){
-  console.log('login page');
+  console.log('welcome to DSM-Home');
   var message = '';
   var sendName = '';
   var sess = req.session; 
