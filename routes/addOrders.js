f194b6d3f2824f4917af2d853f10bedf4030700e
Eladvh <eladvh@gmail.com>
2018-09-26 04:13:50 +0300

    added tracking option via cainiao

--- routes/addOrders.js
+++ routes/addOrders.js
@@ -10,8 +10,11 @@ var sendName = user.first_name + ' ' + user.last_name;
 var itemsNameRes = [];
 var ordersListRes = [];
 var supplierNameRes = [];
+var trakeInfo = [];
 var message = '';
-var answer = {sendName, message, itemsNameRes, supplierNameRes, ordersListRes}
+var id = [];
+var arr= [];
+var answer = {sendName, message, itemsNameRes, supplierNameRes, ordersListRes, trakeInfo}
 
 var userId = req.session.userId;
   
@@ -201,16 +204,78 @@ function asyncFunc2() {
         });
   }
 
+  function asyncFunc3() {
+    return new Promise(
+        function (resolve, reject) {
+            console.log('Get Tracking Details');
+            db.query("SELECT trackNum FROM `tblorders` ORDER BY orderNum DESC", function(err, results, fields)
+            {
+                 if(results.length){
+                    for(var i = 0; i<results.length; i++){
+                        id.push(results[i].trackNum);
+                    }
+
+                    function delay() {
+                        return new Promise(resolve => setTimeout(resolve, 200));
+                       }
+                      async function delayedLog(tracking) {
+                          if(tracking != null){
+                        geartrack.cainiao.getInfo(tracking, (err, TrakerInfo) => {
+                            if(err) { return }
+                            trakeInfo.push(TrakerInfo);
+                            if(TrakerInfo.states.length != 0){
+                                for(var i = TrakerInfo.states.length-1; i > 0 ; i-- ){
+                                    arr[i] = TrakerInfo.states[i].state.split(' ')
+                                    for(var j = 0; j < arr[i].length ; j++){
+                                        //console.log(arr[i][j])
+                                        if(arr[i][j] == 'delivered' || arr[i][j] == 'delivery'){
+                                            var sql = "update tblOrders SET statusOfOrder = ? WHERE trackNum = ?";
+                                            db.query(sql, ['DELIVERED', tracking], function(err, result) {
+                                            })
+                                        }
+                                        if(arr[i][j] == 'transit.' || arr[i][j] == 'Accepted' || arr[i][j] == 'Acceptance'){
+                                            var sql = "update tblOrders SET statusOfOrder = ? WHERE trackNum = ?";
+                                            db.query(sql, ['IN TRANSIT', tracking], function(err, result) {
+                                            })
+                                        }
+                                    }
+                                }
+                                //console.log('-------------------------------------------------------------------------');
+                            }
+                            });
+                        }
+                        
+                        await delay(); 
+                        //console.log(tracking);
+                      }
+                      async function processArray(array) {
+                        for (const tracking in array) {
+                          await delayedLog(array[tracking])
+                        }
+                        //console.log(trakeInfo);
+                      }
+
+                      processArray(id);
+                 }
+            })
+            resolve(trakeInfo);
+          });
+  }
 
 function main() {
     asyncFunc()
     .then(result => {
         answer.supplierNameRes = supplierNameRes;
-      return asyncFunc2();
+        return asyncFunc2();
     })
     .then(result2 => {
         answer.ordersListRes = ordersListRes;
-        res.render('orders_page',{answer:answer});
+        return asyncFunc3();
+    })
+    .then(async result3 => {
+        answer.trakeInfo = trakeInfo;
+        setTimeout(function(){ res.render('orders_page',{answer:answer});  }, 2500);
+        //res.render('orders_page',{answer:answer}); 
     })
     .catch(error => {});
   }
@@ -218,11 +283,33 @@ function main() {
 
 
 
-
   }
 }
 
+  /*async function asyncTask () {
+    try {
+      const valueA = await asyncFunc()
+      answer.supplierNameRes = supplierNameRes;
+      const valueB = await asyncFunc2(valueA)
+      answer.ordersListRes = ordersListRes;
+      const valueC = await asyncFunc3(valueB)
+      console.log(valueC);
+      answer.trakeInfo = trakeInfo;
+      return await asyncFunc3(valueB)
+      //return await functionD(valueC)
+      res.render('orders_page',{answer:answer})
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
+  asyncTask();*/
+
+        /*var courier = tracker.courier(tracker.COURIER.CJKOREAEXPRESS.CODE)
+        var trace_number = 'LY606209059CN';
+        courier.trace(trace_number, function (err, result1) {
+          console.log(result1)
+        })*/
 
 /*let ebay = new Ebay({
     clientID: "EladPint-DSMHome-PRD-0820665a8-01939c18",
@@ -276,3 +363,5 @@ ebay.getAccessToken()
         var obj = JSON.parse(response);
         console.log(obj.variations[0].pricing)})
       .catch(error => console.log(error));*/
+
+      
